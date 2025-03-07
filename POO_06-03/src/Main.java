16e7294329f4f6c5c2692d59c3c813dba68ff6c6
alecioADS <alecio.programador@gmail.com>
2025-03-06 22:25:45 -0300

    Programa de média aritmética.

--- POO_06-03/src/Main.java
+++ POO_06-03/src/Main.java
@@ -1,14 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-    public static String inverterString(String texto) {
-        return new StringBuilder(texto).reverse().toString();
+    public static double calcularMedia(int[] numeros) {
+        int soma = 0;
+        for (int num : numeros) {
+            soma += num;
+        }
+        return (double) soma / numeros.length;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite um texto ou palavra: ");
-        String texto = scanner.nextLine();
-        System.out.println("Texto ou palavra invertido: " + inverterString(texto));
+        System.out.print("Quantos números deseja inserir? ");
+        int tamanho = scanner.nextInt();
+        int[] numeros = new int[tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("Digite o número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        System.out.println("Média aritmética: " + calcularMedia(numeros));
     }
 }
