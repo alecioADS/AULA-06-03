66788c98486b858fa5bbe380546f26a697ae0ebc
alecioADS <alecio.programador@gmail.com>
2025-03-06 22:22:17 -0300

    Programa de inversão de string.

--- POO_06-03/src/Main.java
+++ POO_06-03/src/Main.java
@@ -1,23 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int calcularFatorial(int numero) {
-        int fatorial = 1;
-        for (int i = 1; i <= numero; i++) {
-            fatorial *= i;
-        }
-        return fatorial;
+    public static String inverterString(String texto) {
+        return new StringBuilder(texto).reverse().toString();
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite um número inteiro positivo: ");
-        int num = scanner.nextInt();
-        if (num < 0) {
-            System.out.println("Número inválido! Digite um número positivo.");
-        } else {
-            System.out.println("Fatorial de " + num + " é " + calcularFatorial(num));
-
-        }
+        System.out.println("Digite um texto ou palavra: ");
+        String texto = scanner.nextLine();
+        System.out.println("Texto ou palavra invertido: " + inverterString(texto));
     }
 }
