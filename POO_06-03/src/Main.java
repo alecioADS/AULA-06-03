e03773b01814a102f5d50a8e5d65a17612afa1cc
alecioADS <alecio.programador@gmail.com>
2025-03-06 21:47:13 -0300

    Programa par ou ímpar de um inteiro.

--- POO_06-03/src/Main.java
+++ POO_06-03/src/Main.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int somar(int a, int b){
-        return a + b;
+    public static boolean ehPar(int numero) {
+        return numero % 2 == 0;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o primeiro número: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Digite o segundo número: ");
-        int num2 = scanner.nextInt();
-        System.out.println("Soma: " + somar(num1, num2));
+        System.out.println("Digite um número inteiro: ");
+        int num = scanner.nextInt();
+        System.out.println("O número " + num + " é par? " + ehPar(num));
+
     }
 }
